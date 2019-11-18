a86203004d97846d5ddb359f6cb148a042bc8b54
Diego Villegas <38533871+dgvillegas@users.noreply.github.com>
2019-11-17 23:18:51 -0500

    ProductForm

--- WebPages/FreightTransport_WebPage/forms/js/validations.js
+++ WebPages/FreightTransport_WebPage/forms/js/validations.js
@@ -195,3 +195,43 @@ validarDocumento = function() {
 function searchbyid() {
     var idguide = document.getElementById("idguide").value;
 }
+
+function soloLetras(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    especiales = "8-37-39-46";
+
+    tecla_especial = false
+    for (var i in especiales) {
+        if (key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+        return false;
+    }
+}
+
+function verificaUser() {
+    if (document.user.userU.value == "") {
+        alert("El campo nombre esta vacio");
+        document.getElementById('userU').style.border = 'thin solid #FF0000';
+        return;
+    }
+}
+
+function verificaPassword() {
+    if (document.user.password.value == "") {
+        alert("El campo nombre esta vacio");
+        document.getElementById('password').style.border = 'thin solid #FF0000';
+        return;
+    }
+}
+
+
+function tecla() {
+    alert('TECLADO BLOQUEADO')
+}
