c673134c76403c5c4f0afc581fe8082392484293
dennisesandoval <dasandoval3@espe.edu.ec>
2019-12-01 21:42:40 -0500

    database aws

--- URIS/WebApplications/FreightTransport/src/java/ec/edu/espe/transport/model/DBConnect.java
+++ URIS/WebApplications/FreightTransport/src/java/ec/edu/espe/transport/model/DBConnect.java
@@ -11,14 +11,14 @@ import java.sql.SQLException;
 
 /**
  *
- * @author david
+ * @author Dennise
  */
 public class DBConnect {
     Connection ct;
     String bd = "database_transportes";
-    String url = "jdbc:mysql://localhost/" + bd;
+    String url = "jdbc:mysql://dbinstance.cndtjrwnep9m.us-east-1.rds.amazonaws.com/" + bd;
     String user = "root";
-    String pass = "";
+    String pass = "12345678";
 
     public Connection connect() {
         try {
