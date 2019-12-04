6ab61241c7881136a06f71770eb3e863c3a5dda1
MateoReinoso <mateo_riva@hotmail.com>
2019-12-04 00:18:51 -0500

    get

--- URIS/WebApplications/WebZona/src/java/ec/edu/espe/model/Conexion.java
+++ URIS/WebApplications/WebZona/src/java/ec/edu/espe/model/Conexion.java
@@ -5,10 +5,38 @@
  */
 package ec.edu.espe.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Mateo
  */
 public class Conexion {
+     String bd="prograavanzada";
+    String login="root";
+    String passw="";
+    String url = "jdbc:mysql://localhost:3306/prograavanzada?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     
+    Connection objConex=null;
+    
+    public Conexion (){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            objConex = DriverManager.getConnection(url,login,passw);
+            if(objConex!=null)
+                System.out.println("Conexion a BD ok");
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        } catch (SQLException o){
+            System.out.println(o);
+        }
+    }
+ 
+
+    public Connection obtenerConexion()
+    {
+        return objConex;
+    }
 }
--- URIS/WebApplications/WebZona/src/java/ec/edu/espe/resource/ZoneResource.java
+++ URIS/WebApplications/WebZona/src/java/ec/edu/espe/resource/ZoneResource.java
@@ -5,6 +5,9 @@
  */
 package ec.edu.espe.resource;
 
+import ec.edu.espe.model.ZonaDAO;
+import ec.edu.espe.model.ZonaVO;
+import java.util.ArrayList;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -12,6 +15,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -35,19 +39,13 @@ public class ZoneResource {
      * Retrieves representation of an instance of ec.edu.espe.resource.ZoneResource
      * @return an instance of java.lang.String
      */
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public String getXml() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * PUT method for updating or creating an instance of ZoneResource
-     * @param content representation for the resource
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_XML)
-    public void putXml(String content) {
+        @GET
+    @Path("{codigozona}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<ZonaVO> getJsonProductoPeso(@PathParam("codigozona") int codigozona) {
+        ZonaDAO zdao = new ZonaDAO();
+        ArrayList<ZonaVO> zvo = new ArrayList<>();
+        zvo = zdao.mostrarZonaCodigo(codigozona);
+        return zvo;
     }
 }
