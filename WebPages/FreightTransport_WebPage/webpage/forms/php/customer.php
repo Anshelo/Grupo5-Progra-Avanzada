7a788cf54d98632dae471357c57bbd02672b5225
sdvivas <santi.viv@hotmail.com>
2019-12-12 02:15:51 -0500

    Client php

--- WebPages/FreightTransport_WebPage/webpage/forms/php/customer.php
+++ WebPages/FreightTransport_WebPage/webpage/forms/php/customer.php
@@ -11,11 +11,11 @@ if ($opcion =='Registrar'):
     $cellphoneCustomer = $_POST['fono2customer'];
     $emailCustomer = $_POST['emailCustomer'];
     $addressCustomer = $_POST['addressCustomer'];
-
-    $data =array('ciClient' =>$idCustomer, 'rucClient'=>$rucCustomer,
-                 'nameClient'=>$nameCustomer,'addressClient'=>$addressCustomer,'phoneClient'=>$fonoCustomer,
-                 'mobileClient'=>$cellphoneCustomer,'emailClient'=>$emailCustomer);    
     
+    $data =array('ciClient' =>$idCustomer, 'rucClient'=>$rucCustomer,
+                 'nameClient'=>strval($nameCustomer),'addressClient'=>$addressCustomer,'phoneClient'=>$fonoCustomer,
+                 'mobileClient'=>$cellphoneCustomer,'emailClient'=>$emailCustomer);    
+     
     $cli=curl_init($url);
     curl_setopt($cli, CURLOPT_RETURNTRANSFER, true);
     //curl_setopt($cli, CURLOPT_URL,$url);
@@ -26,32 +26,38 @@ if ($opcion =='Registrar'):
     $respone = curl_exec($cli);
     
     if($respone == false){    
-        echo"<center> <h1>Cliente Si Registrado</h1></center>";
-       echo $nameCustomer;
-    }else{
         echo"<center> <h1>Cliente No Registrado</h1></center>";
-        echo $idCustomer;
+       
+    }else{
+        echo"<center> <h1>Cliente Registrado</h1></center>";
         
     }
-    
+    ?>
+    <br>
+    <center >
+        <a href="../html/Client.html">Regresar</a>
+    </center>
+    <?php
     curl_close($cli);
     
-      elseif ($opcion == 'Buscar'):
+    elseif ($opcion == 'Buscar'):
                 $id = $_POST['idcustomer'];
                 echo " <center><h1>Cliente</h1></center>";
                 // $dataId = json_decode(file_get_contents("http://localhost:1024/FreightTransport/project/client/searchClient/$id"),true);
                 $dataId = json_decode(file_get_contents("http://localhost:8080/FreightTransport/project/client/searchClient/$id"),true);
-                ?>
-				
-				 <html lang="en">
-        <head>
-        <link rel="stylesheet" href="../css/styles.css">
-            <title>Cliente</title>
-        </head>
-        <body>
-        <body class="col-sm-8 main-section mx-auto">
-            <br>
-        <div class="styletittle">
+        if($dataId==null){
+                echo "<center> <h2>No se Encontraron datos</h2></center>";
+        }else{
+            ?> 
+                <html lang="en">
+                <head>
+                <link rel="stylesheet" href="../css/styles.css">
+                  <title>Cliente</title>
+                 </head>
+                 <body>
+                <body class="col-sm-8 main-section mx-auto">
+                    <br>
+                <div class="styletittle">
                                         Datos del cliente
                                     </div><br>
                 <center><table class="table" border="1"  >
@@ -82,8 +88,16 @@ if ($opcion =='Registrar'):
                         ?>
                         </table>
                                 
-                    </center>
-                    <?php 
+                    </center> <br>
+    
+   <?php
+            }
+   ?>
+   <center >
+        <a href="../html/Client.html">Regresar</a>
+    </center>
+   <?php             
+
         elseif ($opcion == 'Eliminar'):
             $idC = $_POST['idcustomer'];
 			//$url = "http://localhost:1024/FreightTransport/project/client/deleteClient/$idC";
@@ -94,8 +108,14 @@ if ($opcion =='Registrar'):
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $response  = curl_exec($ch);
             curl_close($ch);
-            echo"<center> <h2>Cliente eliminado</h2></center>";   
-         elseif ($opcion == 'Modificar'):  
+            echo"<center> <h2>Cliente eliminado</h2></center>";
+    ?>
+    <br>
+    <center >
+        <a href="../html/Client.html">Regresar</a>
+    </center>
+    <?php   
+elseif ($opcion == 'Modificar'):  
             $idCustomer = $_POST['idcustomer'];
             $nameCustomer = $_POST['namecustomer'];
             $rucCustomer = $_POST['ruccustomer'];
@@ -103,15 +123,14 @@ if ($opcion =='Registrar'):
             $cellphoneCustomer = $_POST['fono2customer'];
             $emailCustomer = $_POST['emailCustomer'];
             $addressCustomer = $_POST['addressCustomer'];
-            
+
             $url = "http://localhost:8080/FreightTransport/project/client/updateclient/$idCustomer";
          // $url = "http://localhost:1024/FreightTransport/project/client/updateclient/$idCustomer";  
 
-            
-        
-            $data =array('ciClient' =>$idCustomer, 'rucClient'=>$rucCustomer,
-                         'nameClient'=>$nameCustomer,'addressClient'=>$addressCustomer,'phoneClient'=>$fonoCustomer,
-                         'mobileClient'=>$cellphoneCustomer,'emailClient'=>$emailCustomer);          
+         $data =array('ciClient' =>$idCustomer, 'rucClient'=>$rucCustomer,'nameClient'=>strval($nameCustomer),
+                      'addressClient'=>$addressCustomer,'phoneClient'=>$fonoCustomer,
+                     'mobileClient'=>$cellphoneCustomer,'emailClient'=>$emailCustomer);  
+                
                    
             $data_json = json_encode($data);
             $ch = curl_init();
@@ -120,16 +139,66 @@ if ($opcion =='Registrar'):
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
             curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            
-            $ch  = curl_exec($ch);
-            if($ch == false){    
-                echo"<center> <h1>Cliente Modificado</h1></center>";
-                echo $data_json;
-            }else{
-                echo"<center> <h1>Cliente No Modificado</h1></center>";
-                
-            }
+            $response  = curl_exec($ch);
             curl_close($ch);
+            echo"<center> <h2>Cliente Modificado</h2></center>"; 
+            echo $data_json; 
+
+    ?>
+    <br>
+    <center >
+        <a href="../html/Client.html">Regresar</a>
+    </center>
+    <?php
              
-    endif;
+elseif ($opcion == 'Ver Clientes'):
+                $data = json_decode(file_get_contents("http://localhost:8080/FreightTransport/project/client/clients"),true);
+                ?>
+                <html lang="en">
+                <head>
+                <link rel="stylesheet" href="../css/styles.css">
+                    <title>Guides</title>
+                </head>
+                <body>
+                <body class="col-sm-8 main-section mx-auto">
+                    <br>
+                <div class="styletittle">
+                                                Clientes Registrados
+                                            </div><br>
+                <center><table class="table" border="1" >
+                        <tr class="table-primary">
+                            <td>ci</td>
+                            <td>ruc</td>
+                            <td>nombre</td>
+                            <td>direccion</td>
+                            <td>telefono convencional</td>
+                            <td>telefono celular</td>	
+                            <td>correo</td>	               
+                                    
+                        </tr>
+                        <?php 
+                        foreach ($data as $dataId){
+                        ?>
+                        <tr>
+                            <td><?php echo $dataId['ciClient'] ?></td>
+                            <td><?php echo $dataId['rucClient'] ?></td>
+                            <td><?php echo $dataId['nameClient'] ?></td>
+                            <td><?php echo $dataId['addressClient']?></td>
+                            <td><?php echo $dataId['phoneClient']?></td>
+                            <td><?php echo $dataId['mobileClient'] ?></td>
+                            <td><?php echo $dataId['emailClient']?></td>
+                        </tr>
+                </body>
+                </html>
+                        <?php 
+                        }
+                        ?>
+                        </table>
+                                
+                    </center>
+         <center >
+        <a href="../html/Client.html">Regresar</a>
+         </center>
+    <?php 
+        endif;
  ?>
